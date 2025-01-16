7b2892218d1626b8c414c81e1f38c3272eae93f7
Tom <t.duby@game-sup.fr>
2025-01-16 17:53:06 +0100

    add player controller

---
+++ Source/YADSP/Private/TestCharacterController.cpp
@@ -0,0 +1,40 @@
+﻿#include "TestCharacterController.h"
+#include "DialogueSystem.h"
+#include "DialogueNodeInfoEnd.h"
+#include "Components/TextBlock.h"
+#include "UI/DialoguePlayer.h"
+#include "UI/NewQuestController.h"
+
+DEFINE_LOG_CATEGORY_STATIC(ATestCharacterControllerSub, Log, All);
+
+ATestCharacterController::ATestCharacterController()
+{
+	static ConstructorHelpers::FObjectFinder<UDialogueSystem> DialogueSystemFinder(TEXT("/YADSP/DG_ExampleDialogueGraph"));
+	if (DialogueSystemFinder.Succeeded()) {
+		DialogueSystemPtr = DialogueSystemFinder.Object;
+	} else {
+		UE_LOG(ATestCharacterControllerSub, Error, TEXT("Failed to find dialogue system asset"));
+	}
+}
+
+ATestCharacterController::~ATestCharacterController()
+{
+}
+
+void ATestCharacterController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Play the dialogue
+	DialoguePlayerPtr = NewObject<UDialoguePlayer>(this);
+	DialoguePlayerPtr->PlayDialogue(DialogueSystemPtr, this, [this](EDialogueNodeAction Action, FString Data) {
+		if (NewQuestControllerPtr != nullptr)
+		{
+			NewQuestControllerPtr->RemoveFromParent();
+		}
+		NewQuestControllerPtr = UNewQuestController::CreateInstance(this);
+		NewQuestControllerPtr->QuestText->SetText(FText::FromString(Data));
+		NewQuestControllerPtr->AddToViewport();
+	});
+
+}
---
+++ Source/YADSP/Public/TestCharacterController.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "TestCharacterController.Generated.h"
+
+UCLASS()
+class ATestCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	ATestCharacterController();
+	virtual ~ATestCharacterController();
+
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY()
+	class UDialogueSystem* DialogueSystemPtr = nullptr;
+
+	UPROPERTY()
+	class UDialoguePlayer* DialoguePlayerPtr = nullptr;
+
+	UPROPERTY()
+	class UNewQuestController* NewQuestControllerPtr = nullptr;
+};
